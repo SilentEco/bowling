a999437e7098c7ad0426fd6f48a83b50cd702ebc
KristopherDraperEU <Kristopher.kram@drapereurope.se>
2022-03-22 14:20:33 +0100

    inte riktigt klar

--- components/scoring/index.tsx
+++ components/scoring/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Scoring = () => {
   const [score, setScore] = useState(0);
@@ -8,369 +8,23 @@ const Scoring = () => {
   const [currentThrow, setCurrentThrow] = useState(1);
   const [round, setRound] = useState(1);
   const [spare, setSpare] = useState(false);
+  const [sparePoint, setSparePoint] = useState(0);
   const [strike, setStrike] = useState(false);
-  //#region lite mycket kanske
-  //   if (round <= 10) {
-  //     return (
-  //       <div>
-  //         <h2>Score: {score}</h2>
-  //         <h4>ONE: {firstThrow}</h4>
-  //         <h4>TWO: {secondThrow}</h4>
-  //         <h4>THREE: {thirdThrow}</h4>
+  const [strikeScore1, setStrikeScore1] = useState(0);
+  const [strikeScore2, setStrikeScore2] = useState(0);
 
-  //         <h1>Current Round: {round}</h1>
-  //         <button
-  //           onClick={() => {
-  //             if (round !== 10) {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(1);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 if (spare) {
-  //                   setScore(score + 10);
-  //                   setSecondThrow(10);
+  interface throws {
+    frame: {
+      t1: number;
+      t2: number;
+    };
+  }
 
-  //                   setCurrentThrow(1);
-  //                 } else {
-  //                   setScore(score + (firstThrow + 1));
-  //                   setSecondThrow(1);
-  //                   setCurrentThrow(1);
-  //                 }
-  //                 setSpare(false);
-  //                 setRound(round + 1);
-  //               }
-  //             } else {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(1);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(1);
-  //                 setCurrentThrow(3);
-  //               }
-  //               if (currentThrow === 3) {
-  //                 setScore(score + (firstThrow + secondThrow + 1));
-  //                 setRound(round + 1);
-  //               }
-  //             }
-  //           }}
-  //         >
-  //           1
-  //         </button>
-  //         <button
-  //           onClick={() => {
-  //             if (round !== 10) {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(2);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(2);
-  //                 setCurrentThrow(1);
-
-  //                 setScore(score + (firstThrow + 2));
-  //                 setRound(round + 1);
-  //               }
-  //             } else {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(2);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(2);
-  //                 setCurrentThrow(3);
-  //               }
-  //               if (currentThrow === 3) {
-  //                 setScore(score + (firstThrow + secondThrow + 2));
-  //                 setRound(round + 1);
-  //               }
-  //             }
-  //           }}
-  //         >
-  //           2
-  //         </button>
-  //         <button
-  //           onClick={() => {
-  //             if (round !== 10) {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(3);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(3);
-  //                 setCurrentThrow(1);
-
-  //                 setScore(score + (firstThrow + 3));
-  //                 setRound(round + 1);
-  //               }
-  //             } else {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(3);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(3);
-  //                 setCurrentThrow(3);
-  //               }
-  //               if (currentThrow === 3) {
-  //                 setScore(score + (firstThrow + secondThrow + 3));
-  //                 setRound(round + 1);
-  //               }
-  //             }
-  //           }}
-  //         >
-  //           3
-  //         </button>
-  //         <button
-  //           onClick={() => {
-  //             if (round !== 10) {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(4);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(4);
-  //                 setCurrentThrow(1);
-
-  //                 setScore(score + (firstThrow + 4));
-  //                 setRound(round + 1);
-  //               }
-  //             } else {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(4);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(4);
-  //                 setCurrentThrow(3);
-  //               }
-  //               if (currentThrow === 3) {
-  //                 setScore(score + (firstThrow + secondThrow + 4));
-  //                 setRound(round + 1);
-  //               }
-  //             }
-  //           }}
-  //         >
-  //           4
-  //         </button>
-  //         <button
-  //           onClick={() => {
-  //             if (round !== 10) {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(5);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(5);
-  //                 setCurrentThrow(1);
-
-  //                 setScore(score + (firstThrow + 5));
-  //                 setRound(round + 1);
-  //               }
-  //             } else {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(5);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(5);
-  //                 setCurrentThrow(3);
-  //               }
-  //               if (currentThrow === 3) {
-  //                 setScore(score + (firstThrow + secondThrow + 5));
-  //                 setRound(round + 1);
-  //               }
-  //             }
-  //           }}
-  //         >
-  //           5
-  //         </button>
-  //         <button
-  //           onClick={() => {
-  //             if (round !== 10) {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(6);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(6);
-  //                 setCurrentThrow(1);
-
-  //                 setScore(score + (firstThrow + 6));
-  //                 setRound(round + 1);
-  //               }
-  //             } else {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(6);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(6);
-  //                 setCurrentThrow(3);
-  //               }
-  //               if (currentThrow === 3) {
-  //                 setScore(score + (firstThrow + secondThrow + 6));
-  //                 setRound(round + 1);
-  //               }
-  //             }
-  //           }}
-  //         >
-  //           6
-  //         </button>
-  //         <button
-  //           onClick={() => {
-  //             if (round !== 10) {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(7);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(7);
-  //                 setCurrentThrow(1);
-
-  //                 setScore(score + (firstThrow + 7));
-  //                 setRound(round + 1);
-  //               }
-  //             } else {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(7);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(7);
-  //                 setCurrentThrow(3);
-  //               }
-  //               if (currentThrow === 3) {
-  //                 setScore(score + (firstThrow + secondThrow + 7));
-  //                 setRound(round + 1);
-  //               }
-  //             }
-  //           }}
-  //         >
-  //           7
-  //         </button>
-  //         <button
-  //           onClick={() => {
-  //             if (round !== 10) {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(8);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(8);
-  //                 setCurrentThrow(1);
-
-  //                 setScore(score + (firstThrow + 8));
-  //                 setRound(round + 1);
-  //               }
-  //             } else {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(8);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(8);
-  //                 setCurrentThrow(3);
-  //               }
-  //               if (currentThrow === 3) {
-  //                 setScore(score + (firstThrow + secondThrow + 8));
-  //                 setRound(round + 1);
-  //               }
-  //             }
-  //           }}
-  //         >
-  //           8
-  //         </button>
-  //         <button
-  //           onClick={() => {
-  //             if (round !== 10) {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(9);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(9);
-  //                 setCurrentThrow(1);
-
-  //                 setScore(score + (firstThrow + 9));
-  //                 setRound(round + 1);
-  //               }
-  //             } else {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(9);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(9);
-  //                 setCurrentThrow(3);
-  //               }
-  //               if (currentThrow === 3) {
-  //                 setScore(score + (firstThrow + secondThrow + 9));
-  //                 setRound(round + 1);
-  //               }
-  //             }
-  //           }}
-  //         >
-  //           9
-  //         </button>
-  //         <button
-  //           onClick={() => {
-  //             alert("STRIKE");
-  //             if (round !== 10) {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(10);
-  //                 setCurrentThrow(1);
-  //                 setScore(score + 10);
-  //                 setRound(round + 1);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(10);
-  //                 setCurrentThrow(1);
-  //               }
-  //             } else {
-  //               if (currentThrow === 1) {
-  //                 setFirstThrow(10);
-  //                 setCurrentThrow(2);
-  //               }
-  //               if (currentThrow === 2) {
-  //                 setSecondThrow(10);
-  //                 setCurrentThrow(3);
-  //               }
-  //               if (currentThrow === 3) {
-  //                 setScore(score + (firstThrow + secondThrow + 10));
-  //                 setRound(round + 1);
-  //               }
-  //             }
-  //           }}
-  //         >
-  //           10
-  //         </button>
-  //       </div>
-  //     );
-  //   } else
-  //     return (
-  //       <div>
-  //         <h2>Final score: {score}</h2>
-  //         <h1>GAME OVER</h1>
-  //         <button
-  //           onClick={() => {
-  //             setScore(0);
-  //             setFirstThrow(0);
-  //             setSecondThrow(0);
-  //             setCurrentThrow(1);
-  //             setRound(1);
-  //           }}
-  //         >
-  //           RESTART
-  //         </button>
-  //       </div>
-  //     );
-  // };
-  //#endregion
-
+  let arr: throws[] = [];
   const points = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   if (round <= 10) {
+    console.log(score);
     return (
       <>
         <h1>Score: {score}</h1>
@@ -391,41 +45,121 @@ const Scoring = () => {
               if (currentThrow === 1) {
                 switch (point) {
                   case 10:
-                    setFirstThrow(10);
+                    // setStrike(true);
+                    // setScore(score + point);
+                    // setCurrentThrow(1);
+                    // setRound(round + 1);
                     setScore(10 + score);
-                    setCurrentThrow(1);
+                    setStrike(true);
                     setRound(round + 1);
                     break;
                   default:
                     setFirstThrow(point);
-                    setScore(point + score);
+                    setSparePoint(point);
+                    setScore(score + point);
                     setCurrentThrow(2);
+                    console.log("kast score 1", score);
+                  // arr.push({ frame: { t1: firstThrow, t2: secondThrow } });
+                  // console.log(arr);
                 }
               }
               if (currentThrow === 2) {
                 setSecondThrow(point);
-
-                setScore(point + score);
+                if (firstThrow + point === 10) {
+                  setScore(score + 10);
+                  setSpare(true);
+                } else {
+                  setScore(point + score);
+                  console.log("kast score 2", score);
+                }
                 setCurrentThrow(1);
+                // arr.push({ frame: { t1: firstThrow, t2: secondThrow } });
                 setRound(round + 1);
               }
-              //#region lookback
-              //   if (currentThrow === 1) {
-              //     setFirstThrow(point);
-              //     setCurrentThrow(2);
-              //     setRound(round + 1);
-              //   }
-              //   if (currentThrow === 2) {
-              //     setSecondThrow(point);
-              //     setCurrentThrow(1);
-              //     setRound(round + 1);
-              //   }
-              //#endregion
             };
+            const spareFrame = () => {
+              if (currentThrow === 1) {
+                switch (point) {
+                  case 10:
+                    // setStrike(true);
+                    // setFirstThrow(point);
+                    // setScore(score + point + point);
+                    setScore(10 + score);
+                    setStrike(true);
+                    setRound(round + 1);
+
+                    break;
+                  default:
+                    setFirstThrow(point);
+                    setScore(score + point + point);
+                    setCurrentThrow(2);
+                    console.log("stroke 1", score);
+                  // arr.push({ frame: { t1: firstThrow, t2: secondThrow } });
+                  // console.log(arr);
+                }
+              }
+              if (currentThrow === 2) {
+                setSecondThrow(point);
+                if (firstThrow + point === 10) {
+                  setSpare(true);
+
+                  setScore(score + point);
+                } else {
+                  setScore(point + score);
+                  setSpare(false);
+                }
+                setCurrentThrow(1);
+                // arr.push({ frame: { t1: firstThrow, t2: secondThrow } });
+                setRound(round + 1);
+              }
+            };
+            const strikeFrame = () => {
+              if (currentThrow === 1) {
+                switch (point) {
+                  case 10:
+                    // setStrike(true);
+                    // setStrikeScore1(10);
+                    // setFirstThrow(point);
+                    // setScore(point + score);
+                    // setRound(round + 1);
+                    setScore(10 + score);
+                    setStrike(true);
+                    setRound(round + 1);
+                    setStrikeScore1(point);
+                    break;
+                  default:
+                    setFirstThrow(point);
+                    setStrikeScore1(point);
+                    setCurrentThrow(2);
+                    setScore(point + score + 10);
+                  // arr.push({ frame: { t1: firstThrow, t2: secondThrow } });
+                  // console.log(arr);
+                }
+              }
+              if (currentThrow === 2) {
+                setSecondThrow(point);
+                setStrikeScore2(point);
+
+                if (firstThrow + point === 10) {
+                  setSpare(true);
+                  setScore(10 + score);
+                } else {
+                  setScore(score + point + strikeScore1 + strikeScore2);
+                  setStrike(false);
+                }
+                setCurrentThrow(1);
+
+                // arr.push({ frame: { t1: firstThrow, t2: secondThrow } });
+                setRound(round + 1);
+              }
+            };
+
             const finalFrame = () => {
               if (currentThrow === 1) {
                 switch (point) {
                   case 10:
+                    setStrike(true);
+                    setStrikeScore1(10);
                     setFirstThrow(10);
                     setCurrentThrow(2);
                     break;
@@ -456,6 +190,7 @@ const Scoring = () => {
                   default:
                     setThirdThrow(point);
                     setScore(firstThrow + secondThrow + point + score);
+                    setStrike(false);
                     setRound(round + 1);
                 }
               }
@@ -465,7 +200,13 @@ const Scoring = () => {
               <div key={index}>
                 <button
                   onClick={() => {
-                    round !== 10 ? normalFrame() : finalFrame();
+                    strike
+                      ? strikeFrame()
+                      : spare
+                      ? spareFrame()
+                      : round !== 10
+                      ? normalFrame()
+                      : finalFrame();
                   }}
                 >
                   {point}
@@ -474,6 +215,17 @@ const Scoring = () => {
             );
           })}
         </div>
+        {/* {arr.map(({ frame }, index) => {
+          return (
+            <div key={index}>
+              <p>Round: {index + 1}</p>
+              <hr />
+              <p>t1 {frame.t1}</p>
+              <br />
+              <p>t2 {frame.t2}</p>
+            </div>
+          );
+        })} */}
       </>
     );
   } else
